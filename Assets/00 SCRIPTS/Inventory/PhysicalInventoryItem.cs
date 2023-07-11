237536504c2b64716af3fe5016c3e99b71d39c20
duykhoi1504 <voduykhoi1504@gmail.com>
2023-07-11 21:37:58 +0700

    add new spawn enemy location

--- Assets/00 SCRIPTS/Inventory/PhysicalInventoryItem.cs
+++ Assets/00 SCRIPTS/Inventory/PhysicalInventoryItem.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PhysicalInventoryItem : MonoBehaviour
+public class PhysicalInventoryItem : PowerUp
 {
     // Start is called before the first frame update
     [SerializeField] PlayerInventory playerInventory;
