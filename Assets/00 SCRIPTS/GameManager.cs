5f32b509be572b6143e9e311571cb19ff1add670
duykhoi1504 <voduykhoi1504@gmail.com>
2023-07-12 17:34:44 +0700

    fix poup text and rain system

--- Assets/00 SCRIPTS/GameManager.cs
+++ Assets/00 SCRIPTS/GameManager.cs
@@ -8,29 +8,66 @@ public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] GameObject enemyPrefab;
+    [SerializeField] ParticleSystem rainDrop;
+    [SerializeField] ParticleSystem rainRipples;
+   [SerializeField] float randomTime;
+
+    [SerializeField]float timeCount = 0;
     //float timeCount = 0;
     void Start()
     {
+        EndRain();
         Time.timeScale= 1.0f;
+        randomTime = Random.Range(20, 30);
         //AudioManager.Instant.PlayMusic("VillageTheme");
     }
 
     // Update is called once per frame
     void Update()
     {
-     
 
 
-        //if (timeCount >= 5)
-        //{
-        //    Vector2 ranPos = new Vector2(
-        //        Random.Range(-10, 10),
-        //        Random.Range(-10, 10)
-        //        );
-        //    Instantiate(enemyPrefab, ranPos, Quaternion.identity);
-        //    timeCount = 0;
+        timeCount += Time.deltaTime;
 
-        //}
+        if (timeCount >= randomTime && timeCount < randomTime * 2)
+        {
+            StartRain();
+        }
+        else if (timeCount >= randomTime * 2)
+        {
+            EndRain();
+            timeCount = 0;
+            randomTime = Random.Range(1, 10);
+        }
+
+
+    }
+    public void StartRain()
+    {
+
+        if (!rainDrop.isPlaying)
+        {
+            rainDrop.Play();
+        }
+
+        if (!rainRipples.isPlaying)
+        {
+            rainRipples.Play();
+        }
+
+    }
+    public void EndRain()
+    {
+
+        if (rainDrop.isPlaying)
+        {
+            rainDrop.Stop();
+        }
+
+        if (rainRipples.isPlaying)
+        {
+            rainRipples.Stop();
+        }
 
     }
 }
--- Assets/00 SCRIPTS/TextPoup/TextPoup.cs
+++ Assets/00 SCRIPTS/TextPoup/TextPoup.cs
@@ -6,15 +6,23 @@ public class TextPoup : MonoBehaviour
 {
     // Start is called before the first frame update
 
-  
+    [SerializeField]float timeCount = 0;
     void Start()
     {
+        
        this.transform.localPosition+=new Vector3(0f, 1f, 0f);
-        Destroy(gameObject,2f);
+        
+        //Destroy(gameObject,2f);
 
     }
- 
+    private void Update()
+    {
+        timeCount += Time.deltaTime;
+        if (timeCount > 2f)
+            this.gameObject.SetActive(false);
+    }
+
 
     // Update is called once per frame
-    
+
 }
